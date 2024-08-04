74cea98d13b45c2dd1bdf1a7eaebc42c48264427
roginemy123 <bawigarogine002@gmail.com>
2024-08-05 01:33:08 +0800

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+$(".loader").removeClass("d-none");
+
+$(document).ready(function(){
+    let loader = $(".loader")
+    loader.addClass("d-none");
+    // $("article nav ul li a").click(function(){
+        // let navbarItems = $("article nav ul li a");
+        // let pages = $(".pages")
+        // let target = $(this).attr("data-target")
+
+        // navbarItems.filter(".active").removeClass("active");
+        // $(this).addClass("active");
+
+        // pages.filter(".active").removeClass("active").addClass("d-none");
+        // $(target).removeClass("d-none").addClass("active");
+
+
+    // });
+
+    $(".rooms .content").click(function(){
+        let value = $(this).attr("data-id");
+        loader.removeClass("d-none");
+
+        setTimeout(() => {
+            window.location.href = "view-room.html?view=" + value;
+            loader.addClass("d-none");
+        }, 2000);
+    })
+
+    $(".eat .content").click(function(){
+        let value = $(this).attr("data-id");
+        loader.removeClass("d-none");
+        setTimeout(() => {
+            window.location.href = "view-place.html?view=" + value;
+            loader.addClass("d-none");
+        }, 2000);
+    })
+
+
+});
